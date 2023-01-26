10fd3bbb14d90814bc951ba96dbfb0c318dbb071
nawrrar <122539897+nawrrar@users.noreply.github.com>
2023-01-26 08:30:53 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let Counter = 0
+let Make_it_0 = 0
+let Decrease_by_1 = 0
+input.onButtonPressed(Button.A, function () {
+    Counter += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    Make_it_0 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    Decrease_by_1 += 1
+})
+basic.forever(function () {
+	
+})
